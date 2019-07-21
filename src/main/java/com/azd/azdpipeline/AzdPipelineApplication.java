5946df041d9f22a9c0c2ca3fde249ce4dc439612
Rammohan <rammohan.arabiri@gmail.com>
2019-07-21 13:12:58 +0530

    init commit

---
+++ src/main/java/com/azd/azdpipeline/AzdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.azd.azdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azd/azdpipeline/AzdPipelineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azd.azdpipeline;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AzdPipelineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
